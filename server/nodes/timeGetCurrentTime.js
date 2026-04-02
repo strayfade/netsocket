443146ab01c94aaaec890a73b6d8126a040c2f1e
noah <64668046+strayfade@users.noreply.github.com>
2026-04-02 00:16:42 -0400

    Date built-in nodes

---
+++ server/nodes/timeGetCurrentTime.js
@@ -0,0 +1,16 @@
+const { log, logColors } = require('../log')
+const { number, string, bool } = require('./utils/inputParser')
+
+class NodeDefinition {
+    constructor() {
+        this.addOutput("", "number");
+    }
+}
+NodeDefinition.prototype.title = "Time/Current Timestamp"
+NodeDefinition.prototype.color = "yellow"
+NodeDefinition.prototype.icon = "calendar_clock"
+const NodeFunction = async (node, params, behaviors) => {
+    await behaviors.populateNextNodeLinks([ Date.now() ]);
+    return true
+}
+module.exports = { NodeDefinition, NodeFunction }
